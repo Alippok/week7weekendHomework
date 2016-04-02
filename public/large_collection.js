f112114a1e1b21470428ad4c0fbbeca0f3021b68
A_Lippok <alippok@hotmail.co.uk>
2016-04-02 16:33:20 +0100

    Adds an addObjects function to largecollection object

--- public/large_collection.js
+++ public/large_collection.js
@@ -5,9 +5,14 @@ var LargeCollection = function(){
   this.objects = [];
 }
 
-// LargeCollection.prototype = {
-
-// }
+LargeCollection.prototype = {
+  addObjects: function(){
+    for (var i = 0; i < arguments.length; i++){
+      this.objects.push(arguments[i]);
+    }
+  }
+  
+}
 
 
 module.exports = LargeCollection;
--- specs/large_collection_spec.js
+++ specs/large_collection_spec.js
@@ -5,11 +5,15 @@ var collection = new LargeCollection;
 
 describe("Large Collection", function(){
 
- it("should have an empty object collection", function(){
+  it("should have an empty object holder", function(){
 
-  assert.deepEqual([], collection.objects)
- })
+  assert.deepEqual([], collection.objects);
+ });
 
+  it("should take in multiple arguments and add them to its objects holder", function(){
+    collection.addObjects("Obi-Wan Kenobi", "Luke Skywalker", "Darth Vader")
+    assert.deepEqual(["Obi-Wan Kenobi", "Luke Skywalker", "Darth Vader"], collection.objects)
+  })
 
 
 })
